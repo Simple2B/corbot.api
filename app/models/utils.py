2be0765d54697af28a248829c29dc672c7bcbbc1
viktor <radchenkovictor@yahoo.com>
2020-11-09 11:39:41 +0400

    app skeleton

---
+++ app/models/utils.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class ModelMixin(object):
+    def save(self):
+        # Save this model to the database.
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+
+# Add your own utility classes and functions here.
