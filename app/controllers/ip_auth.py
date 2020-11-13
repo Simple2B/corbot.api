cd8858aa9ad4dd7f1e1e0fd9d4b1a18ba83e4ce0
Rostislav <rostik@ubuntu-s-1vcpu-1gb-fra1-01.(none)>
2020-11-13 17:45:04 +0000

    fix

--- app/controllers/ip_auth.py
+++ app/controllers/ip_auth.py
@@ -1,18 +1,21 @@
 import os
 from functools import wraps
+import distutils.util
 
 from flask import request
 
 # here one has to put all IPs of bots and any other that are going to be used to test this application
 GOOD_IPS = ['127.0.0.1']
 
-IP_AUTH_ENABLED = os.environ.get("IP_AUTH_ENABLED", True)
+
 
 
 def ip_auth(a_func):
 
     @wraps(a_func)
     def wrapTheFunction(*args, **kwargs):
+        IP_AUTH_ENABLED = os.environ.get("IP_AUTH_ENABLED", True)
+        IP_AUTH_ENABLED = bool(distutils.util.strtobool(IP_AUTH_ENABLED))
         ip = request.remote_addr
         if IP_AUTH_ENABLED:
             if ip in GOOD_IPS:
