d227632bc91f1157060400a6037fa9d228d93fbe
Rostislav <rostik@ubuntu-s-1vcpu-1gb-fra1-01.(none)>
2020-12-03 14:30:30 +0000

    major commit

--- app/views/main.py
+++ app/views/main.py
@@ -25,16 +25,17 @@ def api():
     log(log.INFO, "api")
     if request.json:
         json_data = request.json  # automatically converted to dict
+        body = json_data['body']
+        method_name = json_data['subject']
         # dispatch does all work, except converting to json
-        res = make_response(json.dumps(dispatch(json_data), indent=4, sort_keys=True, default=str), 200)
+        res = make_response(json.dumps(dispatch(method_name, body), indent=4, sort_keys=True, default=str), 200)
         res.mimetype = 'application/json'
         return res
     form = TestingForm()  # form to test from admin dashboard
     if form.validate_on_submit():
-        data = json.loads(form.Data.data)  # json is entered to TextArea field called Data
-        # then it it is converted to json to simulate real application
-        res = {"request": form.Method.data, "data": data}
-        res = make_response(json.dumps(dispatch(res), indent=4, sort_keys=True, default=str), 200)
+        body = form.Body.data
+        method_name = form.Subject.data
+        res = make_response(json.dumps(dispatch(method_name, body), indent=4, sort_keys=True, default=str), 200)
         res.mimetype = 'application/json'
         return res
     return {}
